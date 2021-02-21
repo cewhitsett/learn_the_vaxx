61ea620654fa927d8a78c644920bef74c6850a37
cewhitsett <ewhitsett@hmc.edu>
2021-02-21 02:23:31 -0800

    Fix Ciante tabbing

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ def state():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+  return render_template("404.html"), 404
 
 @app.errorhandler(500)
 def internal_server_error():
-    return redirect("500.html"), 500
+  return redirect("500.html"), 500
 
 if __name__ == "__main__":
   app.run(debug=True)
