6347a3fee7a01bc2acabcd449dfcf66a15ec404e
Ciante Jones <ciantej@gmail.com>
2021-02-21 00:16:06 -0800

    Added error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
@@ -9,6 +9,14 @@ def index():
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error():
+    return redirect("500.html"), 500
 
 if __name__ == "__main__":
   app.run(debug=True)
