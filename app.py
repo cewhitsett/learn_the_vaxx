57e02cde4e42373653db1411c280e2d4d914fd20
cewhitsett <ewhitsett@hmc.edu>
2021-02-21 14:52:00 -0800

    Fixes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template
-from data import state_info, responses, reactions
+from data import state_info, responses, reactions, testimonials
 from stats import stats_info
 
 app = Flask(__name__)
@@ -20,6 +20,10 @@ def stats():
 def state():
   return render_template("states.html", state_info=state_info)
 
+@app.route("/testes")
+def tests():
+  return render_template("testimonials.html", testimonials=testimonials)
+
 @app.errorhandler(404)
 def page_not_found(e):
   return render_template("404.html"), 404
@@ -28,20 +32,5 @@ def page_not_found(e):
 def internal_server_error():
   return redirect("500.html"), 500
 
-def get_states():
-  lines = open("extras/state.txt","r").readlines()
-  state_data = {}
-  idx = 0
-  curr_state = ""
-  for line in lines:
-    if idx == 0:
-      curr_state = line.split(".")[1].strip()
-      state_data[curr_state] = []
-    else:
-      url = ".".join(line.split(".")[1:]).strip()
-      state_data[curr_state].append(url)
-    idx = (idx + 1) % 3
-
 if __name__ == "__main__":
-  get_states()
   app.run(debug=True)
